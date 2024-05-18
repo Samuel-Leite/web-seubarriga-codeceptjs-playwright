6a1cff9ce5ff0bcfada6cc602c787a251a3f07d3
Samuel Leite <samuel-leite@outlook.com.br>
2024-05-18 20:02:20 -0300

    refactor(all): 💡 fcorrigir estilo codigo
    correcao dof  formataada formatiacacao do codigo

--- helpers/Hooks.js
+++ helpers/Hooks.js
@@ -1,36 +1,49 @@
-const Helper = require('@codeceptjs/helper')
-const { container, output, helper } = require('codeceptjs')
-const fs = require('fs').promises
-const path = require('path')
+/* eslint-disable linebreak-style */
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable class-methods-use-this */
+/* eslint-disable no-console */
+/* eslint-disable linebreak-style */
+/* eslint-disable import/no-extraneous-dependencies */
+const Helper = require('@codeceptjs/helper');
+const fs = require('fs').promises;
+const path = require('path');
 
 class hooks extends Helper {
   async _init() {
     // before all tests
-    console.log('*************************************')
-    console.log('******* Variáveis de Ambiente *******')
-    console.log('BROWSER: ' + process.env.BROWSER)
+    console.log('*************************************');
+    console.log('******* Variáveis de Ambiente *******');
+    console.log(`BROWSER: ${process.env.BROWSER}`);
     try {
-      await fs.rm(path.resolve(__dirname, '../output'), { recursive: true })
-      console.log('DIRETORIO: excluído com sucesso!')
+      await fs.rm(path.resolve(__dirname, '../output'), { recursive: true });
+      console.log('DIRETORIO: excluído com sucesso!');
     } catch (error) {
-      console.error('DIRETORIO: Ocorreu um erro:', error)
+      console.error('DIRETORIO: Ocorreu um erro:', error);
     }
 
-    console.log('*************************************')
+    console.log('*************************************');
   }
 
-  _before(test) {
-    console.log('--------------------------------Start----------------------------------')
+  _before() {
+    console.log('--------------------------------Start----------------------------------');
   }
+
   _after() {
-    console.log('--------------------------------End----------------------------------')
+    console.log('--------------------------------End----------------------------------');
   } // after a test
+
   _beforeStep() {} // before each step
+
   _afterStep() {} // after each step
+
   _beforeSuite() {} // before each suite
+
   _afterSuite() {} // after each suite
+
   _passed() {} // after a test passed
+
   _failed() {} // after a test failed
+
   _finishTest() {} // after all tests
 }
-module.exports = hooks
+module.exports = hooks;
--- tests/e2e/Login_test.js
+++ tests/e2e/Login_test.js
@@ -1,19 +1,20 @@
-Feature('Acesso a plataforma Seu Barriga')
+/* eslint-disable no-undef */
+Feature('Acesso a plataforma Seu Barriga');
 
-const fs = require('fs')
-const path = require('path')
-const yaml = require('js-yaml')
+const fs = require('fs');
+const path = require('path');
+const yaml = require('js-yaml');
 
 // Carrega as credenciais do arquivo YAML
-const credencialPath = path.resolve(__dirname, '../../resources/data/credencial.yml')
-const credencial = yaml.load(fs.readFileSync(credencialPath, 'utf8'))
+const credencialPath = path.resolve(__dirname, '../../resources/data/credencial.yml');
+const credencial = yaml.load(fs.readFileSync(credencialPath, 'utf8'));
 
 Scenario('Login com credenciais validas', ({ loginPage }) => {
-  loginPage.doLogin(credencial.valida.email, credencial.valida.senha)
-  loginPage.checkLoginStatus('Bem vindo, Seu Madruga!')
-}).tag('wip')
+  loginPage.doLogin(credencial.valida.email, credencial.valida.senha);
+  loginPage.checkLoginStatus('Bem vindo, Seu Madruga!');
+}).tag('wip');
 
 Scenario('Login com credenciais invalidas', ({ loginPage }) => {
-  loginPage.doLogin(credencial.invalida.email, credencial.invalida.senha)
-  loginPage.checkLoginStatus('Problemas com o login do usuário')
-})
+  loginPage.doLogin(credencial.invalida.email, credencial.invalida.senha);
+  loginPage.checkLoginStatus('Problemas com o login do usuário');
+});

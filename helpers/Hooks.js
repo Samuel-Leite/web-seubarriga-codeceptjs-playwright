ea43cfa31ea1d5df83d05886076e6575e0d7ef45
Samuel Leite <samuel-leite@outlook.com.br>
2024-05-18 19:50:32 -0300

    refactor(all): 💡 clean folder
    limpeza de codigo desnecessario

--- helpers/Hooks.js
+++ helpers/Hooks.js
@@ -20,30 +20,17 @@ class hooks extends Helper {
   }
 
   _before(test) {
-    // before a test
-    codeceptjs.container
-      .helpers()
-      .Playwright.browserContext.setGeolocation({ latitude: -23.558, longitude: -46.6597 })
-    codeceptjs.container.helpers().Playwright.browserContext.grantPermissions(['geolocation'])
-    test.retries(process.env.RETRY)
     console.log('--------------------------------Start----------------------------------')
-    // let allure = codeceptjs.container.plugins('allure')
-    // allure.addParameter('0', 'ENV', process.env.BROWSER)
   }
   _after() {
     console.log('--------------------------------End----------------------------------')
   } // after a test
   _beforeStep() {} // before each step
-  _afterStep() {
-    // after each step
-  }
+  _afterStep() {} // after each step
   _beforeSuite() {} // before each suite
   _afterSuite() {} // after each suite
   _passed() {} // after a test passed
   _failed() {} // after a test failed
-  _finishTest() {
-    // after all tests
-    //  execSync('allure serve output', utf8)
-  }
+  _finishTest() {} // after all tests
 }
 module.exports = hooks
